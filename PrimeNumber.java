fff965910399010bc9f9858fef9f47dcf7c076b2
Rekha-15 <rekhapatil.1509@gmail.com>
2021-06-04 19:40:51 -0400

    add : .

---
+++ PrimeNumber.java
@@ -0,0 +1,29 @@
+package com;
+
+import java.util.*;
+import java.util.Scanner;
+
+class PrimeNumber	
+{	
+	 public	static void	main(String[] args)		
+	 {	
+	 	 java.util.Scanner sc = new	java.util.Scanner(System.in);	
+	 	 System.out.println("enter number");	
+	 	 int n = sc.nextInt();	
+	 	 int i;	
+	 	 if(n == 1)	
+	 	 {	
+	 	 System.out.println("Prime starts from 2");	
+	 	 }
+	 	 //checking the prime number
+	 	 for( i = 2; i < n; i++)	
+	 	 {	
+	 	 	 if( n % i == 0)	
+	 	 	 	 System.out.println(n+ " is not a prime number");	
+	 	 	 break;	
+	 	 }
+	 	 //prime or not
+	 	 if(n == i)	
+	 	 	 System.out.println(n+" is a prime number");	
+	 }	
+}	
