8bb1b03043650d638441cc9775d4faa7520681b3
Rekha-15 <rekhapatil.1509@gmail.com>
2021-06-04 20:43:52 -0400

    add : temperaturConversion.java

---
+++ temperaturConversion.java
@@ -0,0 +1,20 @@
+package com;
+import java.util.*;
+
+public class temperaturConversion
+{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the Temperature in Celcius");
+		int Celcius = sc.nextInt();
+		//Converting Celsius to Fahrenheit
+		int Fahrenheit =  (Celcius * 9/5) + 32 ;
+		System.out.println("After Converting: " + Fahrenheit);
+		
+		System.out.println("Enter the Farhenheit");
+		Fahrenheit = sc.nextInt();
+		Celcius =  (Fahrenheit - 32) * 5/9;
+		System.out.println("After Conveting: " + Celcius);
+		
+	}
+}
